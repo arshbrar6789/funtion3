fa08d9ff4292ffeb5259139b04fbadfc2bf9ed5e
arshb <arshb@LAPTOP-KGDE4GHJ>
2022-02-04 23:07:56 -0400

    Add project files.

---
+++ funtion3/Source.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	//char password[] = "fuzzy wuzzy";
+	//printf("The password is\"%s\"\n", password);
+	 float x;
+	printf("Type an float value;");
+	scanf_s("%f", &x);
+	printf("Integer is:%f\n", x);
+
+	return(0);
+
+
+}
